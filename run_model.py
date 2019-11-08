ecbcd71d60ef7bd8c1df559c59223a85f42034ce
Ubuntu <ubuntu@ip-172-31-39-232.ec2.internal>
2019-11-08 21:05:07 +0000

    Test model script

--- run_model.py
+++ run_model.py
@@ -1,25 +1,20 @@
-import sys
 import os
 import numpy as np
-import pandas as pd
 import warnings
-import matplotlib.pyplot as plt
-from pathlib import Path
-
 warnings.filterwarnings('ignore')
-sys.path.append('./src')
 
-from utils import DataGenerator
+from sklearn.metrics import classification_report
+
+from src.datagen import DataGenerator
+from src.utils import get_ytrue_ypred_targets, save_confusion_matrix, save_summary_plots
 
 import tensorflow as tf
-from tensorflow.keras import backend, regularizers
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, Conv2D, Dense, Dropout, \
                                     Activation, MaxPooling2D, Flatten, \
                                     BatchNormalization, GlobalAveragePooling2D
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
-from tensorflow.keras.optimizers import Adam, RMSprop
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.optimizers import Adam
 
 
 
@@ -45,7 +40,7 @@ def build_model():
     model.add(Dense(units = 64, activation = 'relu'))
     model.add(Dropout(0.3))
 
-    model.add(Dense(units = 3, activation = 'softmax'))
+    model.add(Dense(units = 2, activation = 'softmax'))
 
     opt = Adam(lr=0.001)
     model.compile(loss='categorical_crossentropy',
@@ -54,20 +49,25 @@ def build_model():
 
     return model
 
-def get_cb_list():
-    checkpoint_callback = ModelCheckpoint('../models/model2_with_datagen_best_val_loss.h5', 
-                                        monitor='val_loss', mode='min',
-                                        save_best_only=True, verbose=0)
+def get_cb_list(name):
+    earlystop_callback = EarlyStopping(monitor='val_loss', mode='min', 
+                                       patience=6, min_delta=0.005, verbose=1,   
+                                       restore_best_weights=True)
+    
+    checkpoint_callback = ModelCheckpoint(f'./models/{name}_weights_best_val_loss.h5', 
+                                          monitor='val_loss', mode='min',
+                                          save_best_only=True, verbose=1)
 
     reducelr_callback = ReduceLROnPlateau(monitor='val_loss', mode='min', factor=0.8, 
-                                        patience=2, min_delta=0.005, verbose=1)
+                                          patience=2, min_delta=0.005, verbose=1)
 
-    callbacks_list = [checkpoint_callback, reducelr_callback]
+    callbacks_list = [checkpoint_callback, reducelr_callback, earlystop_callback]
 
     return callbacks_list
 
 
-def get_datagens(include=['Rock', 'Hip-Hop'], splits=['training', 'validation', 'test'], 
+def get_datagens(include=['Rock', 'Hip-Hop'], 
+                 splits=['training', 'validation', 'test'], 
                  bs=[64,16,1]):
     datagens = []
     test = False
@@ -76,7 +76,7 @@ def get_datagens(include=['Rock', 'Hip-Hop'], splits=['training', 'validation', 
         if 'test' in split:
             test = True
 
-        datagen = DataGenerator('../data/'+split, include=include, 
+        datagen = DataGenerator('./data/'+split, include=include, 
                                 batch_size=bs, dim=(128,640), 
                                 n_channels=1, test=test)
 
@@ -85,21 +85,34 @@ def get_datagens(include=['Rock', 'Hip-Hop'], splits=['training', 'validation', 
     return tuple(datagens)
 
 
-def main():
+def main(name='model3'):
     # Build model
     model = build_model()
 
     # Get callbacks
-    cbs = get_cb_list()
-    
+    cbs = get_cb_list(name)
+
     # Get datagens
-    train_datagen, valid_datagen, test_datagen = get_datagens()
+    train_dg, valid_dg, test_dg = get_datagens(include=['Rock', 'Hip-Hop'], 
+                                               splits=['training', 'validation', 'test'], 
+                                               bs=[64,16,1])
+
+    # Train
+    history = model.fit_generator(generator=train_dg, epochs=100,
+                                  validation_data=valid_dg, verbose=0, 
+                                  callbacks=cbs)
+
+    # Save
+    model.save(f'./models/{name}_arch_and_weights.h5')
+    save_summary_plots(history, fpath=f'./images/{name}_summary.png')
+    y_true, y_pred, target_names = get_ytrue_ypred_targets(model, test_dg)
+    save_confusion_matrix(y_true, y_pred, target_names, fpath=f'./images/{name}_cm.png')
+    
+    # Print
+    acc = model.evaluate_generator(test_dg)[1]
+    print('Accuracy on test: {:.2f}%\n'.format(acc*100))
+    print(classification_report(y_true, y_pred, target_names=target_names))
 
     
-    history = model.fit_generator(generator=datagen, epochs=25,
-                                  validation_data=valid_datagen, verbose=1, 
-                                  callbacks=callbacks_list)
-
-
 if __name__ == "__main__":
     main()
